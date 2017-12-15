2985e39da757ce3c09d7cc59eac4afbac0c8dfad
Ben Casalino <bencasalino@gmail.com>
2017-12-15 14:19:08 -0700

    updates

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
 const app = express();
-
 const data = require("./data/data");
+const cities = data.cities
+const teams = data.teams
+var result = [];
 
+function combine(data1, data2) {
+  for (let i = 0; i < data1.length; i++) {
+    result.push(data1[i]);
+    for (let j = 0; j < data2.length; j++) {
+      if (result[i].name === data2[j].city) {
+        result[i].team = data2[j].team
+        result[i].division = data2[j].division
+        result[i].website = data2[j].website
+      }
+    }
+  }
+}
+combine(cities, teams);
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.get('/', function (request, response) {
-    response.json(data);
-    // response.json(data.cites);
+app.get('/', function(request, response) {
+  response.json(result);
+  // response.json(data.cites);
 });
 
 app.post("/", (request, response) => {
   response.json(request.body);
 });
 
-
-
-
-app.listen(process.env.PORT || 9000,() => {
+app.listen(process.env.PORT || 9000, () => {
   console.log("this is the app js log");
-}
+});
 
-);
+
+// console.log(combine(cities, teams));
--- data/data.js
+++ data/data.js
@@ -20,17 +20,39 @@ var cities = [{
 
 
 var teams = [{
-  id: 1,
+  city: "denver",
   team: "colorado rapids",
   division: "mls",
   website: "http://www.nba.com/nuggets/"
 }, {
-  id: 2,
+  city: "phoenix",
   team: "phoenix rising fc",
   division: "nasl",
   website: "http://www.phxrisingfc.com/"
+}, {
+  city: "portland",
+  team: "portland timbers",
+  division: "mls",
+  website: "http://www.timbers.com/"
 }];
 
+
+
+// function combine(data1, data2) {
+//   var result = [];
+//   for (let i = 0; i < data1.length; i++) {
+//     result.push(data1[i]);
+//     for (let j = 0; j < data2.length; j++) {
+//       if (result[i].name === data2[j].city) {
+//         result[i].team = data2[j].team
+//         result[i].division = data2[j].division
+//         result[i].website = data2[j].website
+//       }
+//     }
+//   }
+//   return result;
+// }
+// console.log(combine(cities, teams));
 
 module.exports = {
   // jabba,
