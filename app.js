244244c8497386c8fdf8b7fb02f66e60043c2b1c
Ben Casalino <bencasalino@gmail.com>
2017-12-14 20:13:54 -0700

    last update eod

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
+
+const app = express();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
-const app = express();
-
-
+// my data
 const data = require("./data/data");
-
-
+// activate cors and bp?
 app.use(cors());
 app.use(bodyParser.json());
 
-
+// get route in js
 app.get('/', function (request, response) {
     response.json(data.teams);
     response.json(data.cites);
+      console.log("get route log from app.js ");
 });
 
+// post route in js
 app.post("/", (request, response) => {
-  response.json(" heyyyooo ");
+  response.json(" hpost route from app.js ");
 });
 
 
-
-
+//
 app.listen(process.env.PORT || 9000,() => {
-  console.log("this is the app.ls .log");
+  console.log("port is working ");
 }
 
 );
